daa524b7c5e53508cd2eb79ea3d38aed5d0866a2
Takuma Kawazu <kawataku.89love@gmail.com>
2021-08-12 10:46:42 +0900

    modify requests in requirements.txt

--- make_trip/views.py
+++ make_trip/views.py
@@ -21,6 +21,7 @@ from django.conf import settings
 from django.utils.timezone import localtime
 from django.contrib import messages
 from django.views.decorators.csrf import requires_csrf_token
+import requests
 import json
 import traceback
 
@@ -1157,6 +1158,15 @@ def tranSpot(request, num):
 
 @requires_csrf_token
 def my_customized_server_error(request, template_name='500.html'):
-    import traceback
-    print(traceback.format_exc())
+    requests.post(
+        'w1617413633-hou950608.slack.com',
+        data=json.dumps({
+            'text': '\n'.join([
+                f'Request uri: {request.build_absolute_uri()}',
+                traceback.format_exc(),
+            ]),
+            'username': 'Django エラー通知',
+            'icon_emoji': ':jack_o_lantern:',
+        })
+    )
     return HttpResponseServerError('<h1>Server Error (500)だよー</h1>')
