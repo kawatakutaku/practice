a425590e01adb34a83e9b99ee105609d3ba25a7b
Takuma Kawazu <kawataku.89love@gmail.com>
2021-08-13 09:53:01 +0900

    update password

--- Trip_jQuery/settings.py
+++ Trip_jQuery/settings.py
@@ -135,7 +135,7 @@ EMAIL_HOST = 'smtp.googlemail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'kawataku.89love@gmail.com'
-EMAIL_HOST_PASSWORD = 'E30t1so2'
+EMAIL_HOST_PASSWORD = 'tk81win54v6'
 
 # 自作のUserモデルを使用する
 AUTH_USER_MODEL = 'make_trip.User'
