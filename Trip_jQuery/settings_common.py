cfa84be36f95e299c4312e728744425a4f6e6a93
kawatakutaku <kawataku.89love@gmail.com>
2021-09-30 21:32:41 +0900

    よくわからんけど、とりあえず、commit

--- Trip_jQuery/settings_common.py
+++ Trip_jQuery/settings_common.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '01b)%@75kt=-%ayywa)lu&$(e)-(x&6w76_vfgs_d6a(%htz=m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =True
